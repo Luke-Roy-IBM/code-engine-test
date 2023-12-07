edcab8ce7581900388700d75c28af946ffb9f8cb
Luke-Roy-IBM <83647952+Luke-Roy-IBM@users.noreply.github.com>
2023-12-07 08:38:17 +0100

    h1

--- app/main.go
+++ app/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello CodeEngine Playback")
+	fmt.Fprintf(w, "<h1>Hello CodeEngine Playback</h1>")
 }
 
 func main() {
